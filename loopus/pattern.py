90d56ccb3749a265fa425ba40e1e00468549ba80
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-07-13 17:51:02 +0200

    Implement pattern sum

--- loopus/pattern.py
+++ loopus/pattern.py
@@ -25,21 +25,45 @@ def recursive_cycle(iterable):
                 yield element
 
 
-class Pattern(object):
+class AbstractPattern(object):
+
+    def __add__(self, other):
+        if not isinstance(other, AbstractPattern):
+            other = Pattern(other)
+        return PatternOperation(self, other, lambda x, y: x+y)
+
+    def __radd__(self, other):
+        return self + other
+
+
+class Pattern(AbstractPattern):
 
     def __init__(self, pattern):
         self.pattern = pattern if isinstance(pattern, list) else [pattern]
         self.cycle = recursive_cycle(self.pattern)
 
     def __next__(self):
-        n = next(self.cycle)
-        return n
+        return next(self.cycle)
 
     def clone(self):
         return Pattern(self.pattern)
 
     def __repr__(self):
         return f'P{self.pattern}'
+
+
+class PatternOperation(AbstractPattern):
+
+    def __init__(self, p1, p2, operation):
+        self.p1 = p1
+        self.p2 = p2
+        self.operation = operation
+
+    def __next__(self):
+        return self.operation(next(self.p1), next(self.p2))
+
+    def __repr__(self):
+        return f'{self.p1} + {self.p2}'
 
 
 class PatternFactory(object):
--- tests/test_loopus.py
+++ tests/test_loopus.py
@@ -8,6 +8,7 @@ class TestPattern(unittest.TestCase):
         p = Pattern([1, 'A'])
         res = [next(p) for _ in range(10)]
         assert res == [1, 'A', 1, 'A', 1, 'A', 1, 'A', 1, 'A']
+        assert p.__repr__() == "P[1, 'A']"
 
     def test_interlaced_pattern(self):
         p = Pattern(['A', [1, 2]])
@@ -25,8 +26,26 @@ class TestPattern(unittest.TestCase):
         assert res == [1, 'A', 1, 'A', 1, 'A', 1, 'A', 1, 'A']
         assert isinstance(p, Pattern)
 
-    # def test_pattern_sum(self):
-    #     p = P[0, 1, 2]
-    #     s = p + 2
-    #     print(s)
-    #     pass
+    def test_pattern_sum(self):
+        p = P[1, 2] + P[1] + P[1, 2, 3]
+        assert p.__repr__() == 'P[1, 2] + P[1] + P[1, 2, 3]'
+        res = [next(p) for _ in range(10)]
+        assert res == [3, 5, 5, 4, 4, 6, 3, 5, 5, 4]
+
+        p = P[1, 2] + 1
+        res = [next(p) for _ in range(10)]
+        assert res == [2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+
+        p = P[0, 1] + 1 + 1
+        res = [next(p) for _ in range(10)]
+        assert res == [2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+
+        p1 = P[1, 2] + 1
+        p2 = 1 + P[1, 2]
+        for _ in range(10):
+            assert next(p1) == next(p2)
+
+        p1 = P[1] + P[1, 2] + P[1, 2]
+        p2 = P[1] + (P[1, 2] + P[1, 2])
+        for _ in range(10):
+            assert next(p1) == next(p2)
