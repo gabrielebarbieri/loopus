78782afc942c91f4e0f1649acc580426dde9b9d2
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-07-13 14:25:37 +0200

    use scale in player

--- loopus/player.py
+++ loopus/player.py
@@ -5,32 +5,37 @@ from loopus.pattern import Pattern
 
 class Player(Loop):
 
-    def __init__(self, clock, p, dur=None, sus=None, channel=0):
+    def __init__(self, clock, scale, degrees, dur=None, sus=None, channel=0):
         if dur is None:
             dur = 1
         if sus is None:
             sus = dur
         self.channel = channel
-        self.pitches = Pattern(p)
+        self.degrees = Pattern(degrees)
         self.sustains = Pattern(sus)
+        self.scale = scale
         super(Player, self).__init__(clock, durations=dur, actions=None)
 
-    def play_note(self, beat, pitch, dur=1.0, vel=100):
+    def play_note(self, beat, degree, dur=1.0, vel=100):
+        pitch = self.scale[degree].midi
         note = Note(pitch, channel=self.channel)
         self.clock.schedule(beat, note.play, vel)
         self.clock.schedule(beat + dur, note.release)
 
     def schedule_action(self, beat):
         # Need to override this method since play note already schedule playing and stopping a note
-        self.play_note(beat, next(self.pitches), dur=next(self.sustains))
+        self.play_note(beat, next(self.degrees), dur=next(self.sustains))
 
 
 if __name__ == '__main__':
     from loopus.clock import Clock
+    from loopus.scale import Scale
     from time import sleep
     c = Clock()
+    s = Scale()
+
     c.start()
-    p = Player(c, [67, 62, 62], dur=[0.5, 0.25, 0.25], sus=[0.25])
+    p = Player(c, s, [4, 1, 0], dur=[0.5, 0.25, 0.25], sus=[0.25])
     p.play()
     sleep(5)
     p.stop()
