76ea2a6907a2d29e6fb8eca9c1238d547aca8523
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2019-01-04 23:03:41 +0100

    first temporal recursion example

--- loopus/live_client.py
+++ loopus/live_client.py
@@ -12,12 +12,15 @@ class Receiver(object):
     def __init__(self):
         self.dispatcher = dispatcher.Dispatcher()
         # self.dispatcher.set_default_handler(print)
-        self.dispatcher.map('/live/clip/loopjump', self.receive)
+        self.dispatcher.map('/live/clip/state', self.receive)
         self.server = osc_server.ThreadingOSCUDPServer(('127.0.0.1', 9000), self.dispatcher)
         self.q = Queue()
 
     def receive(self, *args):
-        self.q.put(args)
+        print(*args)
+        state = args[3]
+        if state == 2: # clip is_playing
+            self.q.put(args)
 
     def run_server(self):
         t = threading.Thread(target=self.server.serve_forever)
@@ -28,7 +31,6 @@ class Receiver(object):
 
 
 receiver = Receiver()
-receiver.run_server()
 
 
 class Note(object):
@@ -74,57 +76,34 @@ class Clip(object):
         client.send_message('/live/clip/notes/set', msg)
         pass
 
+
+def create_clip(track, scene, notes, length):
+    client.send_message('/live/clip/delete', [track, scene])
+    client.send_message('/live/clip/create', [track, scene, length])
+    c = Clip(track, scene)
+    c.set_notes(notes)
+    c.play()
+
+
 if __name__ == "__main__":
 
-    notes = [Note(p, i, 1.) for i, p in enumerate([55, 55, 57, 59])]
-    for i in range(4):
-        notes.append(Note(52 - 12, i+0.5, 1))
+    receiver.run_server()
 
-    c = Clip(1, 0)
-    c.play()
-    import time
-    time.sleep(0.1)
+    def get_notes(bass):
+        notes = [Note(p, i, 1.) for i, p in enumerate([55, 55, 57, 59])]
+        for i in range(4):
+            notes.append(Note(bass - 12, i+0.5, 1))
+        return notes
 
-    client.send_message('/live/clip/loopjump', [1, 1])
+    basses = [52, 50, 48, 48, 48]
 
+    def play(clip=0):
+        create_clip(0, clip, get_notes(basses[clip]), 4)
+        print(receiver.q.get())
+        if clip + 1 < len(basses):
+            play(clip+1)
 
-    input('hello')
-    #
-    # Clip(1, 1).play()
-    # print(receiver.q.get())
-    # Clip(1, 0).play()
-    # print(receiver.q.get())
-    # Clip(1, 1).play()
-    # print(receiver.q.get())
-    # Clip(1, 0).play()
-    # print(receiver.q.get())
-    # Clip(1, 1).play()
-    # print(receiver.q.get())
-
-
-
-
-    # c = Clip(0, 0)
-    # c.play()
-    # import time
-    # time.sleep(0.1)
-    # c = Clip(1, 1)
-    # c.play()
-
-
-
-    # c.set_notes(notes)
-    # input('play any key to stop')
-    # c.stop()
+    play()
 
     client.send_message('/live/stop', 'query')
     receiver.stop_server()
-
-    # c.stop()
-    # client.send_message("/live/clip/stop", [1, 0])
-    # client.send_message('/live/clip/state', [1, 0])
-
-    # notes = [1, 0, 52, 0.5, 0.5, 100, 0, 52, 1.5, 0.5, 100, 0, 52, 2.5, 0.5, 100, 0, 52, 3.5, 0.5, 100, 0, 55, 0.0, 1.0,
-    #          100, 0, 55, 1.0, 1.0, 100, 0,
-    #          57, 2.0, 1.0, 100, 0, 59, 3.0, 1.0, 100, 0]
-    # client.send_message('/live/clip/notes/add', notes)
